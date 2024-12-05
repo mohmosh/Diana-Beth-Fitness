e248be77f18e8205b2184a2107888773b570e9a7
mohmosh <moshwanjiku@gmail.com>
2024-12-05 11:16:40 +0300

    DBF view

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Diana Beth Fitness</title>
+<!--
+
+TemplateMo 548 Training Studio
+
+https://templatemo.com/tm-548-training-studio
+
+-->
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-training-studio.css">
+
+    </head>
+
+    <body>
+
+
+
+ <!-- ***** Header Area Start ***** -->
+ <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">Diana Beth<em> Fitness</em></a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                        <li class="scroll-to-section"><a href="#features">About</a></li>
+                        <li class="scroll-to-section"><a href="#our-classes">Classes</a></li>
+                        <li class="scroll-to-section"><a href="#schedule">Schedules</a></li>
+                        <li class="scroll-to-section"><a href="#contact-us">Contact</a></li>
+                        <li class="main-button"><a href="#">Sign Up</a></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <!-- Include the login and register modals -->
+    @include('auth.login')
+    @include('auth.register')
+   
+<!-- ***** Header Area End ***** -->
